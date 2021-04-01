a2e4d8e5ee28a68c7b2bb1ee79fefe7ba5db9fc0
Emerson <emerson.monteguti@gmail.com>
2021-04-01 20:06:35 -0300

    adicionado formulario vazio

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function helloWorld() {
     var inputs = document.getElementsByTagName('input');
     var input = inputs[0];
-    var nome = input.value;
+    var nome = input.value.trim();
+
+    if(nome ===''){
+        alert('O nome esta vazio, por favor insira o seu nome');
+        return
+    }
+    console.log('Nome: ', nome)
     window.alert('Hello world,' + nome + '!');
     input.value = '';
 }
